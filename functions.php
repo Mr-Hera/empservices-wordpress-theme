fc74ba1c51ba55ba26766c60ff68eae4ececd4ed
Mr-Hera <randellhera@gmail.com>
2023-04-05 17:19:16 +0300

    Feat: dynamically set header stylesheets & footer scripts

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+    function empservices_register_styles() {
+        $version = wp_get_theme()->get('Version');
+        wp_enqueue_style('empservices-style', get_template_directory_uri(). "/style.css", array('empservices-bootstrap'), $version, 'all');
+        wp_enqueue_style('empservices-bootstrap', get_template_directory_uri(). "/assets/css/bootstrap/css/bootstrap.min.css", array(), '1.0', 'all');
+        wp_enqueue_style('empservices-aos', get_template_directory_uri(). "/assets/css/aos/css/aos.css", array(), '1.0', 'all');
+        wp_enqueue_style('empservices-remixicon', get_template_directory_uri(). "/assets/css/remixicon/css/remixicon.css", array(), '1.0', 'all');
+        wp_enqueue_style('empservices-swiper', get_template_directory_uri(). "/assets/css/swiper/css/swiper-bundle.min.css", array(), '1.0', 'all');
+        wp_enqueue_style('empservices-glightbox', get_template_directory_uri(). "/assets/css/glightbox/css/glightbox.min.css", array(), '1.0', 'all');
+    }
+
+    add_action('wp_enqueue_scripts', 'empservices_register_styles');
+
+    function empservices_register_scripts() {
+        wp_enqueue_script('empservices-bootstrap', get_template_directory_uri(). "/assets/js/bootstrap/js/bootstrap.bundle.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-aos', get_template_directory_uri(). "/assets/js/aos/aos.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-php-email-form', get_template_directory_uri(). "/assets/js/php-email-form/validate.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-swiper', get_template_directory_uri(). "/assets/js/swiper/swiper-bundle.min.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-purecounter', get_template_directory_uri(). "/assets/js/purecounter/purecounter.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-isotope-layout', get_template_directory_uri(). "/assets/js/isotope-layout/isotope.pkgd.min.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-glightbox', get_template_directory_uri(). "/assets/js/glightbox/js/glightbox.min.js", array(), '1.0', true);
+        wp_enqueue_script('empservices-main', get_template_directory_uri(). "/assets/js/main/main.js", array(), '1.0', true);
+    }
+
+    add_action('wp_enqueue_scripts', 'empservices_register_scripts');
+?>
